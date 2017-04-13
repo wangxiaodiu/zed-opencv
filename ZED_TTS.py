1f65817a2d986eca844617a982c0270280fe4a2f
Liang Niu <niuliang3@gmail.com>
2017-04-13 11:56:29 -0400

    add TTS python script

---
+++ ZED_TTS.py
@@ -0,0 +1,34 @@
+# install pyttsx and libgcc
+import pyttsx
+import time
+time.sleep(1)
+
+def obj_pairing(line):
+    obj, depth = line.strip().split(',')
+    return [obj, float(depth)/100]
+
+def sentence_synth(obj_label, obj_depth, side_margin=0, dist_unit='meters'):
+    obj_label = obj_label
+    obj_depth = obj_depth
+
+    sntnc = 'There is a %s %.1f %s ahead.'%(obj_label, obj_depth, dist_unit)
+
+    if side_margin > 0:
+        sntnc += (' %.1f %s to your right.'%(side_margin, dist_unit))
+    elif side_margin < 0:
+        sntnc += (' %.1f %s to your left.'%(side_margin, dist_unit))
+    return sntnc
+
+# sound = engine.getProperty('voices')
+# engine.setProperty('voice', sound[16].id)
+
+path = '/tmp/'
+with open(path + 'depth.txt') as file_object:
+    lines = file_object.readlines()
+
+obj_dict = dict(map(obj_pairing, lines))
+obj_label, obj_depth = sorted(obj_dict.items(), key=lambda x: x[1])[0]
+
+engine = pyttsx.init()
+engine.say(sentence_synth(obj_label, obj_depth))
+engine.runAndWait()
