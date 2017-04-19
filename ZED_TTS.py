b2588cdd890d12c0faa6eee84c9944eece988112
Liang Niu <niuliang3@gmail.com>
2017-04-19 15:23:25 -0400

    new TTS from Cheng

--- ZED_TTS.py
+++ ZED_TTS.py
@@ -1,28 +1,38 @@
 # install pyttsx and libgcc
 import pyttsx
 import time
-time.sleep(1)
 
 def obj_pairing(line):
     obj, depth = line.strip().split(',')
     return [obj, float(depth)/100]
 
-def sentence_synth(obj_label, obj_depth, side_margin=0, dist_unit='meters'):
+def sentence_synth(obj_label, obj_depth, side_margin=0, out_unit='meters'):
+    '''
+    This function receives the info regarding the closest object in screen
+        obj_label -- object label
+        obj_depth -- object depth
+        side_margin -- optional, states the horizontal location, default 0
+        out_unit -- output distance unit, can be meters or feet, default meters
+    (Assuming the input distance unit is 'meter'.)
+    '''
     obj_label = obj_label
     obj_depth = obj_depth
-
-    sntnc = 'There is a %s %.1f %s ahead.'%(obj_label, obj_depth, dist_unit)
+    
+    if out_unit.lower() == 'feet':
+        obj_depth *= 3.28084
+        side_margin *= 3.28084
+    sntnc = 'There is a %s %.1f %s ahead.'%(obj_label, obj_depth, out_unit)
 
     if side_margin > 0:
-        sntnc += (' %.1f %s to your right.'%(side_margin, dist_unit))
+        sntnc += (' %.1f %s to your right.'%(side_margin, out_unit))
     elif side_margin < 0:
-        sntnc += (' %.1f %s to your left.'%(side_margin, dist_unit))
+        sntnc += (' %.1f %s to your left.'%(side_margin, out_unit))
     return sntnc
 
 # sound = engine.getProperty('voices')
 # engine.setProperty('voice', sound[16].id)
 
-path = '/tmp/'
+path = '/home/cheng/Documents/ZED_Project/'
 while True:
     with open(path + 'depth.txt') as file_object:
         lines = file_object.readlines()
@@ -31,8 +41,9 @@ while True:
     obj_label, obj_depth = sorted(obj_dict.items(), key=lambda x: x[1])[0]
 
     engine = pyttsx.init()
-    engine.say(sentence_synth(obj_label, obj_depth))
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-50)
+    engine.say(sentence_synth(obj_label, obj_depth, out_unit='feet'))
     engine.runAndWait()
     engine = None
-
-    time.sleep(2)
+    time.sleep(5)
